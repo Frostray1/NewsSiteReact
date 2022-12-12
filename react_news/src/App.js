65a184ef7d781a334530135aff09ff97e2920568
Artem <a.bychkoff1@yandex.ru>
2022-12-13 01:01:28 +0300

    Создано react приложение

---
+++ react_news/src/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+
+
+
+    </div>
+  );
+}
+
+export default App;
